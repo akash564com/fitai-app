e0cba70c1c2b803c79c62f5867e7bdcbae1fa187
Akash <akashkalanspn564@gmail.com>
2025-08-01 18:25:40 +0530

    Update LanguageSwitcher.tsx

---
+++ components/LanguageSwitcher.tsx
@@ -0,0 +1,11 @@
+// components/LanguageSwitcher.tsx
+import React from "react";
+
+export default function LanguageSwitcher() {
+  return (
+    <div>
+      <button>EN</button>
+      <button>HI</button>
+    </div>
+  );
+}
